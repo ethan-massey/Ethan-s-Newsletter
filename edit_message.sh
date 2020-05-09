fe9e4a159106e1cd24811e39cac612fec1039ecc
ethan-massey <44252355+ethan-massey@users.noreply.github.com>
2020-05-09 14:01:56 -0400

    Update edit_message.sh

--- edit_message.sh
+++ edit_message.sh
@@ -8,3 +8,7 @@ old_message=`sed '6q;d' email_part_2.html`
 
 # replace old message with new one
 sed -i -e s/"${old_message}"/"${new_message}"/g email_part_2.html
+
+# print edited html file
+echo HTML file with changes:
+cat email_part_2.html
