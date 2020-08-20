ba4e8dfc9050488a61a419fe8c663c7a545da809
ethan-massey <44252355+ethan-massey@users.noreply.github.com>
2020-08-20 13:34:02 -0400

    Update main.py to check internet connection

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
-import email_manager
 import datetime as dt
+import urllib.request
 
 
-def send_email():
-    email_manager.send_message()
-    print(str(dt.datetime.now()) + ' - email sent', flush=True)
+# connected to internet?
+def is_connected(host='http://google.com'):
+    try:
+        urllib.request.urlopen(host)
+        return True
+    except:
+        return False
 
 
 def main():
+    if is_connected():
+        # send email
+        import email_manager
+        email_manager.send_message()
+        print(str(dt.datetime.now()) + ' - email sent', flush=True)
+    else:
+        # print error message
+        print(str(dt.datetime.now()) + ' - ERROR: No internet connection', flush=True)
 
-    send_email()
 
 if __name__ == "__main__":
     main()
